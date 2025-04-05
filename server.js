cb054a13091407ef5e5d956f7d3f800fcd413c69
laptar-ls <taras.lapytskyi@lembergsolutions.com>
2025-04-05 15:53:00 +0300

    hw26

--- server.js
+++ server.js
@@ -4,7 +4,15 @@ import os from 'os';
 const hostname = os.hostname();
 const port = 80;
 
+function cpuStress() {
+  const endTime = Date.now() + 1000;
+  while (Date.now() < endTime) {
+    Math.sqrt(Math.random());
+  }
+}
+
 const server = http.createServer((req, res) => {
+  cpuStress();
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end(`Host name: \n---\n${hostname}\n---`);
