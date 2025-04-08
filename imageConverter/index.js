0bdc656227febc1f60163ffabea37535c4b41098
laptar-ls <taras.lapytskyi@lembergsolutions.com>
2025-04-08 17:04:07 +0300

    hw27

--- imageConverter/index.js
+++ imageConverter/index.js
@@ -2,7 +2,6 @@ import { S3Client, GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3
 import sharp from "sharp";
 import path from "path";
 
-// AWS SDK v3 клієнт
 const s3 = new S3Client({ region: 'eu-central-1' });
 
 export const handler = async (event) => {
@@ -10,23 +9,21 @@ export const handler = async (event) => {
     const record = event.Records[0];
     const bucket = record.s3.bucket.name;
     const key = decodeURIComponent(record.s3.object.key.replace(/\+/g, " "));
+    console.log('key', key)
 
-    // Завантаження зображення з S3
     const getObjectCommand = new GetObjectCommand({ Bucket: bucket, Key: key });
     const { Body } = await s3.send(getObjectCommand);
 
-    // Отримуємо весь файл в буфер
-    const originalImage = Buffer.from(await Body.arrayBuffer());
+    const byteArray = await Body.transformToByteArray();
+    const originalImage = Buffer.from(byteArray);
 
     const baseName = path.basename(key, path.extname(key));
-    const newKey = `converted/${baseName}.bmp`;
+    const newKey = `converted-${baseName}.png`;
 
-    // Конвертація в BMP
     const bmpImage = await sharp(originalImage)
       .toFormat("bmp")
       .toBuffer();
 
-    // Завантаження в S3
     const putObjectCommand = new PutObjectCommand({
       Bucket: bucket,
       Key: newKey,
